6c4eb6fccb518ce280c620f458918c25586842f8
Ran Arino <ranarino0611ca@gmail.com>
2025-08-01 21:33:10 +0900

    feat(auth): create layout and pages for sign-in and sign-up using Clerk components

---
+++ frontend/src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,20 @@
+import { SignIn } from "@clerk/nextjs";
+
+export default function Page() {
+  return (
+    <SignIn 
+      appearance={{
+        elements: {
+          rootBox: {
+            width: "100%",
+          },
+          card: {
+            width: "100%",
+            boxShadow: "0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)",
+            borderRadius: "8px",
+          },
+        }
+      }}
+    />
+  );
+}
