266cd001979e5f8d6aa3e26dcee031cfce5bc099
Ran Arino <ranarino0611ca@gmail.com>
2025-08-16 11:30:38 +0900

    feat(main): start gRPC server on service initialization

--- ms_document_process/app/main.py
+++ ms_document_process/app/main.py
@@ -1,5 +1,7 @@
+from app.server.grpc_server import serve
 from app.config.config import settings
 
 if __name__ == "__main__":
     print("ms_document_process service started")
     print("Settings loaded successfully.")
+    serve()
