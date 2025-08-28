bf20b1e4cdef874c02954983271426a054320f10
Ran Arino <ranarino0611ca@gmail.com>
2025-08-28 11:31:21 +0900

    feat(knowledge): add GenerateDownloadURL RPC for generating temporary download URLs and update proto file paths

--- ms_knowledge/api/proto/v1/knowledge_grpc.pb.go
+++ ms_knowledge/api/proto/v1/knowledge_grpc.pb.go
@@ -2,7 +2,7 @@
 // versions:
 // - protoc-gen-go-grpc v1.5.1
 // - protoc             v5.29.3
-// source: ms_knowledge/api/proto/v1/knowledge.proto
+// source: api/proto/v1/knowledge.proto
 
 package knowledgev1
 
@@ -30,6 +30,7 @@ const (
 	KnowledgeService_GetContentSource_FullMethodName          = "/knowledge.v1.KnowledgeService/GetContentSource"
 	KnowledgeService_ListContentSources_FullMethodName        = "/knowledge.v1.KnowledgeService/ListContentSources"
 	KnowledgeService_UpdateContentSourceStatus_FullMethodName = "/knowledge.v1.KnowledgeService/UpdateContentSourceStatus"
+	KnowledgeService_GenerateDownloadURL_FullMethodName       = "/knowledge.v1.KnowledgeService/GenerateDownloadURL"
 	KnowledgeService_CreateKnowledgeLink_FullMethodName       = "/knowledge.v1.KnowledgeService/CreateKnowledgeLink"
 	KnowledgeService_GetKnowledgeLink_FullMethodName          = "/knowledge.v1.KnowledgeService/GetKnowledgeLink"
 	KnowledgeService_ListKnowledgeLinks_FullMethodName        = "/knowledge.v1.KnowledgeService/ListKnowledgeLinks"
@@ -58,6 +59,8 @@ type KnowledgeServiceClient interface {
 	GetContentSource(ctx context.Context, in *GetContentSourceRequest, opts ...grpc.CallOption) (*ContentSource, error)
 	ListContentSources(ctx context.Context, in *ListContentSourcesRequest, opts ...grpc.CallOption) (*ListContentSourcesResponse, error)
 	UpdateContentSourceStatus(ctx context.Context, in *UpdateContentSourceStatusRequest, opts ...grpc.CallOption) (*ContentSource, error)
+	// Download URL Generation
+	GenerateDownloadURL(ctx context.Context, in *GenerateDownloadURLRequest, opts ...grpc.CallOption) (*GenerateDownloadURLResponse, error)
 	// Knowledge Graph Management
 	CreateKnowledgeLink(ctx context.Context, in *CreateKnowledgeLinkRequest, opts ...grpc.CallOption) (*KnowledgeLink, error)
 	// Single enriched link by id
@@ -181,6 +184,16 @@ func (c *knowledgeServiceClient) UpdateContentSourceStatus(ctx context.Context, 
 	return out, nil
 }
 
+func (c *knowledgeServiceClient) GenerateDownloadURL(ctx context.Context, in *GenerateDownloadURLRequest, opts ...grpc.CallOption) (*GenerateDownloadURLResponse, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(GenerateDownloadURLResponse)
+	err := c.cc.Invoke(ctx, KnowledgeService_GenerateDownloadURL_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 func (c *knowledgeServiceClient) CreateKnowledgeLink(ctx context.Context, in *CreateKnowledgeLinkRequest, opts ...grpc.CallOption) (*KnowledgeLink, error) {
 	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
 	out := new(KnowledgeLink)
@@ -279,6 +292,8 @@ type KnowledgeServiceServer interface {
 	GetContentSource(context.Context, *GetContentSourceRequest) (*ContentSource, error)
 	ListContentSources(context.Context, *ListContentSourcesRequest) (*ListContentSourcesResponse, error)
 	UpdateContentSourceStatus(context.Context, *UpdateContentSourceStatusRequest) (*ContentSource, error)
+	// Download URL Generation
+	GenerateDownloadURL(context.Context, *GenerateDownloadURLRequest) (*GenerateDownloadURLResponse, error)
 	// Knowledge Graph Management
 	CreateKnowledgeLink(context.Context, *CreateKnowledgeLinkRequest) (*KnowledgeLink, error)
 	// Single enriched link by id
@@ -331,6 +346,9 @@ func (UnimplementedKnowledgeServiceServer) ListContentSources(context.Context, *
 }
 func (UnimplementedKnowledgeServiceServer) UpdateContentSourceStatus(context.Context, *UpdateContentSourceStatusRequest) (*ContentSource, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method UpdateContentSourceStatus not implemented")
+}
+func (UnimplementedKnowledgeServiceServer) GenerateDownloadURL(context.Context, *GenerateDownloadURLRequest) (*GenerateDownloadURLResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GenerateDownloadURL not implemented")
 }
 func (UnimplementedKnowledgeServiceServer) CreateKnowledgeLink(context.Context, *CreateKnowledgeLinkRequest) (*KnowledgeLink, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method CreateKnowledgeLink not implemented")
@@ -557,6 +575,24 @@ func _KnowledgeService_UpdateContentSourceStatus_Handler(srv interface{}, ctx co
 	return interceptor(ctx, in, info, handler)
 }
 
+func _KnowledgeService_GenerateDownloadURL_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(GenerateDownloadURLRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(KnowledgeServiceServer).GenerateDownloadURL(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: KnowledgeService_GenerateDownloadURL_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(KnowledgeServiceServer).GenerateDownloadURL(ctx, req.(*GenerateDownloadURLRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 func _KnowledgeService_CreateKnowledgeLink_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(CreateKnowledgeLinkRequest)
 	if err := dec(in); err != nil {
@@ -749,6 +785,10 @@ var KnowledgeService_ServiceDesc = grpc.ServiceDesc{
 			Handler:    _KnowledgeService_UpdateContentSourceStatus_Handler,
 		},
 		{
+			MethodName: "GenerateDownloadURL",
+			Handler:    _KnowledgeService_GenerateDownloadURL_Handler,
+		},
+		{
 			MethodName: "CreateKnowledgeLink",
 			Handler:    _KnowledgeService_CreateKnowledgeLink_Handler,
 		},
@@ -782,5 +822,5 @@ var KnowledgeService_ServiceDesc = grpc.ServiceDesc{
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
-	Metadata: "ms_knowledge/api/proto/v1/knowledge.proto",
+	Metadata: "api/proto/v1/knowledge.proto",
 }
