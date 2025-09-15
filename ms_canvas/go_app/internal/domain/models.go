9f92cf16a4232f65f7654af360660dce05deae60
Ran Arino <ranarino0611ca@gmail.com>
2025-09-15 15:48:24 +0900

    feat(models): add ContentNode struct and ContentRepository interface for content management

---
+++ ms_canvas/go_app/internal/domain/models.go
@@ -0,0 +1,17 @@
+package domain
+
+import (
+	"time"
+	"github.com/google/uuid"
+)
+
+type ContentNode struct {
+	ID              uuid.UUID
+	ContentSourceID uuid.UUID
+	CreatedAt       time.Time
+	UpdatedAt       time.Time
+}
+
+type ContentRepository interface {
+	CreateContentNode(node ContentNode) error
+}
