ce0e818c14d7540294ebbf9ff0f0cec592b353f8
Ran Arino <ranarino0611ca@gmail.com>
2025-09-27 16:50:28 +0900

    refactor(knowledge): update proto file references and add new fields to UpdateContentSourceStatusRequest for content summary, keywords, and title

--- ms_knowledge/api/proto/v1/knowledge_grpc.pb.go
+++ ms_knowledge/api/proto/v1/knowledge_grpc.pb.go
@@ -2,7 +2,7 @@
 // versions:
 // - protoc-gen-go-grpc v1.5.1
 // - protoc             v5.29.3
-// source: api/proto/v1/knowledge.proto
+// source: knowledge.proto
 
 package knowledgev1
 
@@ -624,5 +624,5 @@ var KnowledgeService_ServiceDesc = grpc.ServiceDesc{
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
-	Metadata: "api/proto/v1/knowledge.proto",
+	Metadata: "knowledge.proto",
 }
